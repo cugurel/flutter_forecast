244a9c1203895c0699537f6675b76e4f8cd1b55f
Çağrı Uğurel <cugurel7@gmail.com>
2021-11-16 23:48:03 +0300

    Smartgit Test

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -12,6 +12,7 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   String city = 'Ankara';
   int? temperature;
+  
   var woeid;
   var locationData;
   String abbr = 'c';
